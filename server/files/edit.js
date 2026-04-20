e82cbfb821e7ef1feccc9b55088c54e967f57330
oezkan <oezkan.oez@stud.hcw.ac.at>
2026-04-20 02:43:49 +0200

    commit

--- server/files/edit.js
+++ server/files/edit.js
@@ -1,9 +1,8 @@
 function setMovie(movie) {
-
   for (const element of document.forms[0].elements) {
     const name = element.id;
     const value = movie[name];
-    
+
     if (name === "Genres") {
       const options = element.options;
       for (let index = 0; index < options.length; index++) {
@@ -19,7 +18,9 @@ function setMovie(movie) {
 function getMovie() {
   const movie = {};
 
-  const elements = Array.from(document.forms[0].elements).filter(element => element.id)
+  const elements = Array.from(document.forms[0].elements).filter(
+    (element) => element.id,
+  );
 
   for (const element of elements) {
     const name = element.id;
@@ -35,9 +36,17 @@ function getMovie() {
           value.push(option.value);
         }
       }
-    } else if (name === "Metascore" || name === "Runtime" || name === "imdbRating") {
-        value = Number(element.value);
-    } else if (name === "Actors" || name === "Directors" || name === "Writers") {
+    } else if (
+      name === "Metascore" ||
+      name === "Runtime" ||
+      name === "imdbRating"
+    ) {
+      value = Number(element.value);
+    } else if (
+      name === "Actors" ||
+      name === "Directors" ||
+      name === "Writers"
+    ) {
       value = element.value.split(",").map((item) => item.trim());
     } else {
       value = element.value;
@@ -52,33 +61,36 @@ function getMovie() {
 function putMovie() {
   const movie = getMovie();
 
-  const xhr = new XMLHttpRequest()
-  xhr.onload = function() {
-    if (xhr.status == 200 || xhr.status === 204) {
-      location.href = 'index.html'
+  const xhr = new XMLHttpRequest();
+  xhr.open("PUT", "/movies/" + movie.imdbID);
+  xhr.setRequestHeader("Content-Type", "application/json");
+
+  xhr.onload = function () {
+    if (xhr.status == 200 || xhr.status === 201 || xhr.status === 204) {
+      location.href = "index.html";
     } else {
-      alert("Saving of movie data failed. Status code was " + response.status)
+      alert("Saving of movie data failed. Status code was " + xhr.status);
     }
-  }
-  
-  xhr.open("PUT", "/movies/" + movie.imdbID)
-  xhr.setRequestHeader("Content-Type", "application/json")
+  };
 
-  xhr.send(JSON.stringify(movie))
-
+  xhr.send(JSON.stringify(movie));
 }
 
-/** Loading and setting the movie data for the movie with the passed imdbID */
 const imdbID = new URLSearchParams(window.location.search).get("imdbID");
 
 const xhr = new XMLHttpRequest();
 xhr.open("GET", "/movies/" + imdbID);
-xhr.onload = function() {
+xhr.onload = function () {
   if (xhr.status === 200) {
     setMovie(JSON.parse(xhr.responseText));
   } else {
-    alert("Loading of movie data failed. Status was " + xhr.status + " - " + xhr.statusText);
-  } 
-}
+    alert(
+      "Loading of movie data failed. Status was " +
+        xhr.status +
+        " - " +
+        xhr.statusText,
+    );
+  }
+};
 
-xhr.send()
+xhr.send();
--- server/server.js
+++ server/server.js
@@ -5,8 +5,8 @@ const movieModel = require('./movie-model.js');
 
 const app = express();
 
-// Parse urlencoded bodies
-app.use(bodyParser.json()); 
+// Parse json bodies
+app.use(bodyParser.json());
 
 // Serve static content in directory 'files'
 app.use(express.static(path.join(__dirname, 'files')));
@@ -15,44 +15,52 @@ app.use(express.static(path.join(__dirname, 'files')));
    This endpoint returns a sorted array of all the genres of the movies
    that are currently in the movie model.
 */
+app.get('/genres', function (req, res) {
+  const genres = [...new Set(Object.values(movieModel).flatMap((movie) => movie.Genres))];
+  genres.sort();
+  res.send(genres);
+});
 
 /* Task 1.4: Extend the GET /movies endpoint:
    When a query parameter for a specific genre is given, 
    return only movies that have the given genre
  */
 app.get('/movies', function (req, res) {
-  let movies = Object.values(movieModel)
+  let movies = Object.values(movieModel);
+
+  if (req.query.genre) {
+    movies = movies.filter((movie) => movie.Genres.includes(req.query.genre));
+  }
+
   res.send(movies);
-})
+});
 
 // Configure a 'get' endpoint for a specific movie
 app.get('/movies/:imdbID', function (req, res) {
-  const id = req.params.imdbID
-  const exists = id in movieModel
- 
+  const id = req.params.imdbID;
+  const exists = id in movieModel;
+
   if (exists) {
-    res.send(movieModel[id])
+    res.send(movieModel[id]);
   } else {
-    res.sendStatus(404)    
+    res.sendStatus(404);
   }
-})
+});
 
 app.put('/movies/:imdbID', function(req, res) {
+  const id = req.params.imdbID;
+  const exists = id in movieModel;
 
-  const id = req.params.imdbID
-  const exists = id in movieModel
+  movieModel[id] = req.body;
 
-  movieModel[req.params.imdbID] = req.body;
-  
   if (!exists) {
-    res.status(201)
-    res.send(req.body)
+    res.status(201);
+    res.send(req.body);
   } else {
-    res.sendStatus(200)
+    res.sendStatus(200);
   }
-  
-})
+});
 
-app.listen(3000)
+app.listen(3000);
 
 console.log("Server now listening on http://localhost:3000/")
